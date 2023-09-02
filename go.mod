ec62a2b9e7eb21754d14a7450c034de7a87ac646
Snowball_233 <wangrb.qq@outlook.com>
2023-09-02 09:54:59 +0800

    feat: Import env file support

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module FrostyAssistant
 
 go 1.19
 
-require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/joho/godotenv v1.5.1
+)
 
 require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"FrostyAssistant/components"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	// Set up Telegram API
-	bot, err := tgbotapi.NewBotAPI("YOUR_BOT_TOKEN")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_HTTP_API"))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -19,7 +27,6 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
